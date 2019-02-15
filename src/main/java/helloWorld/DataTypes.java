244179994e7a61f9662d9c08e7dd14b8d003c3ad
Darek Strojecki <dstroj@wp.pl>
2019-02-15 16:42:20 +0100

    Initial commit

---
+++ src/main/java/helloWorld/DataTypes.java
@@ -0,0 +1,22 @@
+package helloWorld;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        System.out.print("Liczba int:");
+        System.out.println(25);
+
+        System.out.print("Liczba float:");
+        System.out.println(25.45f);
+
+        System.out.print("Znak:");
+        System.out.println('a');
+
+        System.out.print("Bool:");
+        System.out.println(true);
+
+
+
+    }
+}
