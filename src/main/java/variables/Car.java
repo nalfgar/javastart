c39f0d9fd1b6e4ee9c43a63d4d3902080cac2a1e
Darek Strojecki <dstroj@wp.pl>
2019-02-15 17:34:51 +0100

    Add classes in variable module.

---
+++ src/main/java/variables/Car.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class Car {
+
+
+    public static void main(String[] args) {
+        String brand = "Toyota";
+        String type = "Yaris";
+        int year = 2016;
+        float price = 125_000;
+
+        System.out.println("Brand: " + brand);
+        System.out.println("Type: " + type);
+        System.out.println("Year: " + year);
+        System.out.println("Price: " + price);
+    }
+}
---
+++ src/main/java/variables/Variables.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        String name;
+        name = "Darek";
+        System.out.println(name);
+
+        int age = 40;
+        System.out.println(age + 40);
+
+        var x = "Napis:";
+        var y = 50000;
+
+        System.out.println(x + y);
+    }
+}
