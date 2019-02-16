d8710a0782fda6dec39012748d6dc8ec1226e220
Darek Strojecki <dstroj@wp.pl>
2019-02-16 17:18:02 +0100

    Add Calculor and Calculate class. (Homework task)

---
+++ src/main/java/methodsAndConstructors/Calculate.java
@@ -0,0 +1,16 @@
+package methodsAndConstructors;
+
+public class Calculate {
+
+    public static void main(String[] args) {
+
+        double a = 12.5;
+        double b = 32.5;
+
+        System.out.println(Calculator.add(a, b));
+        System.out.println(Calculator.subtract(a, b));
+        System.out.println(Calculator.multiply(a, b));
+        System.out.println(Calculator.divide(a, b));
+
+    }
+}
