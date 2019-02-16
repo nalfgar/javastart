b33cb0a155f98a9d114a78565406519435271768
Darek Strojecki <dstroj@wp.pl>
2019-02-16 16:48:22 +0100

    Add class Employee and Company. Homework task.

---
+++ src/main/java/classAndObjects/Company.java
@@ -0,0 +1,14 @@
+package classAndObjects;
+
+public class Company {
+
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Aaaa", "Bbbb", 1978, 2);
+        Employee employee2 = new Employee("Ccccc", "Ddddd", 1988, 12);
+        Employee employee3 = new Employee("Fffff", "Ggggg", 1990, 10);
+
+        System.out.println(employee1);
+        System.out.println(employee2);
+        System.out.println(employee3);
+    }
+}
