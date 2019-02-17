6818b206e6d0cef35363a154432f121e5ed53655
Darek Strojecki <dstroj@wp.pl>
2019-02-17 21:12:02 +0100

    Small changes in Array class.

--- src/main/java/arrays.java
+++ src/main/java/arrays.java
@@ -2,16 +2,21 @@ public class arrays {
 
     public static void main(String[] args) {
 
-        int[] array = new int[1000];
+        int[] arrayA = new int[1000];
+        int[] arrayB = new int[1000];
         int sum = 0;
 
-        for (int i = 0; i < array.length; i++) {
-            array[i] = i*i;
+        for (int i = 0; i < arrayA.length; i++) {
+            arrayA[i] = i*i;
+            arrayB[i] = i;
         }
 
-        for (int i1 : array) {
-            sum += i1;
-            System.out.println(i1);
+        for (int i : arrayA) {
+            sum += i;
+        }
+
+        for (int i : arrayB) {
+            sum += i;
         }
 
         System.out.println(sum);
