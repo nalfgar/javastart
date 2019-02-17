3866cce391227fbcfa5df615200d49d72e94f5f9
Darek Strojecki <dstroj@wp.pl>
2019-02-17 13:52:54 +0100

    Add CalculatorX class and test for it. (Homework task)

---
+++ src/main/java/overloading/CalculatorX.java
@@ -0,0 +1,21 @@
+package overloading;
+
+public class CalculatorX {
+
+    public static int add(int a, int b, int c) {
+        return a+b+c;
+    }
+
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a-b;
+    }
+
+    public static int subtract(int a, int b, int c) {
+        return a - (b+c);
+    }
+}
