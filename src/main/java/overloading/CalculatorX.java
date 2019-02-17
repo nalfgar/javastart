784d63e1954163a5c2d771a1ca674160fa923210
Darek Strojecki <dstroj@wp.pl>
2019-02-17 15:49:56 +0100

    Small changes in CalculatorX class.

--- src/main/java/overloading/CalculatorX.java
+++ src/main/java/overloading/CalculatorX.java
@@ -3,19 +3,19 @@ package overloading;
 public class CalculatorX {
 
     public static int add(int a, int b, int c) {
-        return a+b+c;
+        return a + b + c;
     }
 
 
     public static int add(int a, int b) {
-        return a+b;
+        return a + b;
     }
 
     public static int subtract(int a, int b) {
-        return a-b;
+        return a - b;
     }
 
     public static int subtract(int a, int b, int c) {
-        return a - (b+c);
+        return a - (b + c);
     }
 }
